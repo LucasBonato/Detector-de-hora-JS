bdcaea3bcfa26114d1cf04fb081c7a60e414b9ae
Lucas Bonato <95046421+LucasBonato@users.noreply.github.com>
2022-04-25 03:43:34 -0300

    resolução

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 
 function carregar() {
     let msg = document.querySelector('div#msg');
-    let foto = document.querySelector('div#foto');
+    let foto = document.querySelector('img#imagem');
     let data = new Date();
-    let hora = 18;//data.getHours();
+    let hora = data.getHours();
     msg.innerHTML = `Agora são ${hora} horas.`;
     if (hora >= 6 && hora < 12) {
         //Boa manhã
-        foto.src = 'foto-manha.png';
+        foto.src = 'img/foto-manha.png';
+        document.body.style.background = '#e2cd9f';
     } else if (hora > 12 && hora < 18) {
         //Boa tarde
-        foto.src = 'foto-tarde.png';
+        foto.src = 'img/foto-tarde.png';
+        document.body.style.background = '#b9846f';
     } else {
         //Boa noite
-        foto.src = 'foto-noite.png';
+        foto.src = 'img/foto-noite.png'
+        document.body.style.background = '#2e3896';
     }
 }
